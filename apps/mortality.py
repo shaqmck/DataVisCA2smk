ef346467d6c9166d967316a160e6ceb0bf00961e
shaqmck <35528046+shaqmck@users.noreply.github.com>
2023-05-02 03:14:42 +0100

    Update mortality.py

--- apps/mortality.py
+++ apps/mortality.py
@@ -10,7 +10,7 @@ import plotly.express as px
 
 from app import app
 
-covid = pd.read_csv(r'C:\Users\shaqu\OneDrive\Desktop\Masters Course\DataVis\CA2\covid_data.csv')
+covid = pd.read_csv(r'https://raw.githubusercontent.com/shaqmck/DataVisCA2smk/covid_data.csv')
 covid1 =covid
 covid1['date'] = pd.to_datetime(covid1['date'])
 #covid['month_year'] = pd.to_datetime(covid['month_year'],format='%Y-%m').dt.to_period('M') 
